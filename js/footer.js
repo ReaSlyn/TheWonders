f2c20eec6d76b460c380bbed974ec3e611f74ede
ReaSlyn <reaslynn@gmail.com>
2021-06-16 13:43:53 +0200

    mercredi midi

---
+++ js/footer.js
@@ -0,0 +1,12 @@
+var z = location.pathname.substring(location.pathname.lastIndexOf('/')+1);
+window.onload = () => { document.querySelectorAll(".icon_footer").forEach((element) => {
+
+    var id = element.id.substring(element.id.lastIndexOf('_')+1);
+    var path_maj = z.replace('.html', '');
+    var path = path_maj.toLowerCase()
+    console.log(id, path)
+    if ( id == path ) {
+        element.className += ' active';
+    }
+});
+}
