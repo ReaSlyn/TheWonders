6954c45c71067249e35b5c35b7fe58b1896eda8a
ReaSlyn <reaslynn@gmail.com>
2021-06-17 14:47:50 +0200

    jeudi midi

--- js/insta_token.js
+++ js/insta_token.js
@@ -1,4 +1,4 @@
-$.get('https://ig.instant-tokens.com/users/e67e08e3-2101-4f6d-9f3a-8dc9d3a3627e/instagram/17841403438699060/token?userSecret=cscdsmr5ehfm6q7ubyval9', returntoken);
+$.get('https://ig.instant-tokens.com/users/3b01bf60-b1d5-402c-b751-5245a9c33399/instagram/17841403438699060/token?userSecret=j0qliqeh0ui0sz5ue6p0sok', returntoken);
 function returntoken(data) {
     let data_json = JSON.parse(data);
     console.log(data_json.Token);
