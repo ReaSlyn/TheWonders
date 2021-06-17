8191bb192b24a71ffa95bfd38ab3c1babc1d00bd
ReaSlyn <reaslynn@gmail.com>
2021-06-17 22:42:41 +0200

    jeudi 23h

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,30 @@
-const menuBtn = document.querySelector('.menu-btn');
+const menu_button = document.querySelector('.burger_menu_button');
+const menu = document.querySelector('#modal');
+const blur = document.querySelector('#blur');
+const close_button = document.querySelector('#close');
 let menuOpen = false;
-menuBtn.addEventListener('click', () => {
+menu_button.addEventListener('click', () => {
   if(!menuOpen) {
-    menuBtn.classList.add('open');
+    menu_button.classList.add('open');
+    menu.classList.add('opened');
+    blur.classList.add('active');
     menuOpen = true;
-  } else {
-    menuBtn.classList.remove('open');
+  }});
+
+close_button.addEventListener('click', () => {
+    if(menuOpen) {
+    menu_button.classList.remove('open');
+    menu.classList.remove('opened');
+    blur.classList.remove('active');
+    menuOpen = false;
+  }
+});
+
+blur.addEventListener('click', () => {
+    if(menuOpen) {
+    menu_button.classList.remove('open');
+    menu.classList.remove('opened');
+    blur.classList.remove('active');
     menuOpen = false;
   }
 });
