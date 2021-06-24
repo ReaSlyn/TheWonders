1f6beb875642411a418b4a7dbedbf7ac8978c1eb
Chris <60599836+Chriiiiiss@users.noreply.github.com>
2021-06-24 03:43:11 +0200

    :rocket: {ADD} History html css & js

---
+++ js/challenge.js
@@ -0,0 +1,37 @@
+const menu_element = document.querySelectorAll(".title-div")
+const quotidien = document.querySelector(".quotidien")
+const duo = document.querySelector(".duo")
+const hebdo = document.querySelector(".hebdo")
+
+menu_element.forEach(element => {
+    element.addEventListener("click", (e) => {
+        let menu = element.outerText.toLowerCase();
+
+        menu_element.forEach(element => {
+            if(element.classList.contains("selected")) {
+
+                element.classList.toggle("selected")
+            }
+        })
+
+        element.classList.toggle("selected")
+
+        if (element.outerText.toLowerCase() === "quotidien") {
+            quotidien.classList.remove("hidden")
+            duo.classList.add("hidden")
+            hebdo.classList.add("hidden")
+        }
+        if (element.outerText.toLowerCase() === "duo") {
+            duo.classList.remove("hidden")
+            quotidien.classList.add("hidden")
+            hebdo.classList.add("hidden")
+        }
+
+        if (element.outerText.toLowerCase() === "hebdo") {
+            hebdo.classList.remove("hidden")
+
+            quotidien.classList.add("hidden")
+            duo.classList.add("hidden")
+        }
+    })
+});
